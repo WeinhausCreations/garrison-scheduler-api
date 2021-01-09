906ceac20dcd7088c711e6af8e8132c822fac1af
Scott Hubert <weinhauscreations@gmail.com>
2021-01-09 12:36:22 -0700

    added some useless stuff maybe

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -39,8 +39,69 @@ router.get("/section/:id/checkedIn", (req, res) => {
         [id],
         (err, rows, fields) => {
             if (err) throw err;
-            console.log(rows);
             res.status(200).json(rows);
+        }
+    );
+});
+
+router.get("/section/:id/occupancy/current", (req, res) => {
+    let id = req.params.id;
+    db.query(
+        "SELECT COUNT(*) AS occupancy FROM reservation WHERE checked_in IS NOT NULL AND checked_out IS NULL",
+        [id],
+        (err, rows, fields) => {
+            if (err) throw err;
+            res.status(200).json(rows[0].currentCount);
+        }
+    );
+});
+
+router.get("/section/:id/occupancy/future", (req, res) => {
+    let id = req.params.id;
+    let result = { timeSlot: [], occupancy: [] };
+    let currDate = new Date();
+    db.query(
+        "SELECT * FROM service_section_hours WHERE section_id = ? AND day_of_week = ?",
+        [id, new Date().getDay()],
+        (err, rows, fields) => {
+            if (err) throw err;
+            console.log(rows)
+            if (rows[0].closed === 0) {
+                const openHour = parseInt(rows[0].open_time)
+                const closeHour = parseInt(rows[0].close_time)
+                for (let i = openHour; i < closeHour; i += 0.5) {
+                    let date = new Date(
+                        currDate.getFullYear(),
+                        currDate.getMonth(),
+                        currDate.getDate(),
+                        i,
+                        i % 1 === 0 ? 0 : 30,
+                        0
+                    );
+                    let timeSlot = `${
+                        date.getHours() < 10
+                            ? "0" + date.getHours()
+                            : date.getHours()
+                    }:${
+                        date.getMinutes() < 10
+                            ? "0" + date.getMinutes()
+                            : date.getMinutes()
+                    }`;
+                    db.query(
+                        "SELECT ? AS time_slot, COUNT(*) AS occupancy FROM reservation WHERE section_id = ? AND start <= ? AND stop > ? AND cancelled = 0 AND status_id = 1",
+                        [timeSlot, id, date, date],
+                        (err, rows, fields) => {
+                            if (err) throw err;
+                            result.timeSlot.push(timeSlot);
+                            result.occupancy.push(rows[0].occupancy);
+                            if (i === closeHour - 0.5)
+                                res.status(200).json(result);
+                        }
+                    );
+                }
+            } else {
+                res.status(200).json({ status: 200, message: "closed" });
+            }
         }
     );
 });
